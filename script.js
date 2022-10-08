3b4b717e3c1560c601fb936c11b73468bf5263c6
thiagosmorais <94266257+thiagosmorais@users.noreply.github.com>
2022-10-07 22:45:35 -0300

    Update script.js

--- script.js
+++ script.js
@@ -23,18 +23,35 @@ function criaCoresAleatorias (){
     const g = Math.floor(Math.random() * 255);
     const b = Math.floor(Math.random() * 255);
     return `rgb(${r}, ${g}, ${b})`;
+    
 }
 
 function pintaPaleta (){
     const colocaCor = document.getElementsByClassName('color')
+    
+    for (let index = 1; index < 4; index += 1){
+               
+        if (localStorage.getItem(`colorPallete${index}`) === null) {
+            console.log('cor nova criada');
+            let cor = criaCoresAleatorias();
+            colocaCor[index].style.backgroundColor = cor;
+            localStorage.setItem(`colorPallete${index}`,cor);
+            console.log(cor);
+            
+        } else {
+            console.log('cor carregada');
+            colocaCor[index].style.backgroundColor = localStorage.getItem(`colorPallete${index}`); 
+            console.log(localStorage.getItem(`colorPallete${index}`)); 
 
-    for (let index = 1; index <colocaCor.length; index += 1){
-        colocaCor[index].style.backgroundColor = criaCoresAleatorias();
+
+        }
+
     }
-    return colocaCor;
+    console.log(localStorage);
+    //return colocaCor;
+    
+    
 }
-
-
 
 function criaQuadro() {
     const getQuadro = document.getElementById('pixel-board');
@@ -58,11 +75,13 @@ function criaQuadro() {
 }
 
 
+
+
 // chamar funções
 criaPaleta();
 criaCoresAleatorias();
 pintaPaleta();
-console.log(pintaPaleta());
+//console.log(pintaPaleta());
 criaQuadro();
 
 
